f30bc012e40b192a4c229a8030477a1651049a4f
Jon Roby <jonathan.roby1@gmail.com>
2016-12-15 21:39:26 -0800

    try different format

--- CLRS/Chapter01/Exercises/1.1.5.py
+++ CLRS/Chapter01/Exercises/1.1.5.py
@@ -1,3 +1,12 @@
-# Come up with a real-world problem in which only the best solution will do. Then come up with one in which a solution that is "approximately" the best is good enough.
+'''
 
-# A approximately sorted dictionary would be mostly unhelpful, though an approximate shortest distance would be good enough.
+Question
+Come up with a real-world problem in which only the best solution will do. Then
+come up with one in which a solution that is "approximately" the best is good
+enough.
+
+Answer
+A approximately sorted dictionary would be mostly unhelpful, though an
+approximate shortest distance would be good enough.
+
+'''
