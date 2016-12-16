b2eddeaf4875fd0577b97ef4318843c2d618fdcf
Jon Roby <jonathan.roby1@gmail.com>
2016-12-15 21:20:37 -0800

    Exercises 1.1.x

---
+++ CLRS/Chapter01/Exercises/1.1.3.py
@@ -0,0 +1,3 @@
+# Select a data structure that you have seen previously an discuss its strengths and limitations.
+
+# Linked List. One advantage is that its length can increase and decrease. One disadvantage is that retrieval is O(n)
---
+++ CLRS/Chapter01/Exercises/1.1.4.py
@@ -0,0 +1,3 @@
+# How are the shortest-path and traveling-salesman problems given above similar? How are they different?
+
+# Both involve travelling through a graph and finding a path. In the travelling salesman problem, the path must return to the starting position. In shortest-path, you don't.
