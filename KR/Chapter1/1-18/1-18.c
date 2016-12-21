e2cc494f0cc0141fd3993d8086b3ff0b67975e15
Jon Roby <jonathan.roby1@gmail.com>
2016-12-20 22:27:15 -0800

    KR 1-18

---
+++ KR/Chapter1/1-18/1-18.c
@@ -0,0 +1,60 @@
+// Write a program to remove trailing blanks and tabs from each line of input, and to delete
+// entirely blank lines.
+
+// The current solution inserts a tab, rather than an empty string, which is apparently
+// impossible to do in C.
+
+#include <stdio.h>
+#define MAXLINE 1000
+
+int get_line(char line[], int maxline);
+void copy(char to[], char from[]);
+void trim_line(char to[], char from[]);
+
+int main() {
+  int len;
+  char line[MAXLINE];
+  char trimmed_line[MAXLINE];
+
+  while ((len = get_line(line, MAXLINE)) > 0) {
+    trim_line(trimmed_line, line);
+    printf("%s \n", trimmed_line);
+  }
+
+  return 0;
+}
+
+int get_line(char s[], int lim) {
+  int c, i;
+
+  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+    s[i] = c;
+  }
+
+  if (c == '\n') {
+    s[i] = c;
+    ++i;
+  }
+  s[i] = '\0';
+  return i;
+}
+
+void trim_line(char to[], char from[]) {
+  int i;
+  for (i = 0; from[i] != '\0'; ++i) {
+    if (from[i] == '\t' || from[i] == ' ') {
+      to[i] = '\t';
+    } else {
+      to[i] = from[i];
+    }
+  }
+}
+
+
+void copy(char to[], char from[]) {
+  int i;
+
+  i = 0;
+  while ((to[i] = from[i]) != '\0')
+    ++i;
+}
