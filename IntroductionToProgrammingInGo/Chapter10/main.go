0bec30a2a82c7e5c1e9fcae25928e157a214e2c9
Jon Roby <jonathan.roby1@gmail.com>
2017-03-08 23:21:44 -0800

    chapter 10

---
+++ IntroductionToProgrammingInGo/Chapter10/main.go
@@ -0,0 +1,40 @@
+// package main
+//
+// import "fmt"
+//
+// func f(n int) {
+//   for i := 0; i < 10; i++ {
+//     fmt.Println(n, ":", i)
+//   }
+// }
+//
+// func main() {
+//   go f(0)
+//   var input string
+//   fmt.Scanln(&input)
+// }
+
+package main
+
+import (
+  "fmt"
+  "time"
+  "math/rand"
+)
+
+func f(n int) {
+  for i := 0; i < 10; i++ {
+    fmt.Println(n, ":", i)
+    amt := time.Duration(rand.Intn(20000))
+    time.Sleep(time.Millisecond * amt)
+  }
+}
+
+func main() {
+  for i := 0; i < 10; i++ {
+    go f(i)
+  }
+
+  var input string
+  fmt.Scanln(&input)
+}
